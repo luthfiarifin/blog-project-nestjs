28746d9b20b1c7deb95db08dedc5786d9183b110
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-12-18 23:48:52 +0700

    Database Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConnectionService } from './database-connection.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: DatabaseConnectionService }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
